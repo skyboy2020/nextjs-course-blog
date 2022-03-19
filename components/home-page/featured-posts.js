fc17d46c8f9fb9dd29d4610e4902bfe882ecb7f8
skyboy2020 <89569970@qq.com>
2022-03-19 18:31:35 +0800

    add lib dir

--- components/home-page/featured-posts.js
+++ components/home-page/featured-posts.js
@@ -1,6 +1,7 @@
 import PostsGrid from '../posts/posts-grid';
 import classes from './featured-posts.module.css';
 
+//精选博客主题
 function FeaturedPosts(props) {
   return (
     <section className={classes.latest}>
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import { Fragment } from "react"
 import Hero from '../components/home-page/hero'
+import FeaturedPosts from '../components/home-page/featured-posts'
 
 function HomePage() {
   return (
     <Fragment>
       {/* 顶部个人介绍组件 */}
       <Hero />
+      {/* 精选博客主题 */}
       <FeaturedPosts />
     </Fragment>
   )
