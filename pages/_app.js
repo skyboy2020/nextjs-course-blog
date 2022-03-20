9bb36f838104a556b12506a410a1622e2914d639
skyboy2020 <89569970@qq.com>
2022-03-20 15:52:18 +0800

    finished _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 
 import '../styles/globals.css';
-import Layout from '../components/layout/layout';
+import Layout from '../layout/layout';
 
 function MyApp({ Component, pageProps }) {
   return (
